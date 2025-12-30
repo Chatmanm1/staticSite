a544ed105cdb4ce0acdfee9dc3690fa35475a2a1
Matcha <96433082+Chatmanm1@users.noreply.github.com>
2025-12-30 08:24:22 -0600

    Update generate_blog_index.js

--- scripts/generate_blog_index.js
+++ scripts/generate_blog_index.js
@@ -31,6 +31,7 @@ const html = `<!DOCTYPE html>
   <title>Blog Posts</title>
   <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://matchamakes.net/style.css" />
+  <link rel="stylesheet" href="/style.css" />
 </head>
 <body>
 
