329102615409d1f2ca75aa08cf0a26e088244346
Matcha <96433082+Chatmanm1@users.noreply.github.com>
2025-12-30 08:15:41 -0600

    Update generate_blog_index.js

--- scripts/generate_blog_index.js
+++ scripts/generate_blog_index.js
@@ -18,12 +18,43 @@ const links = files.map(f =>
 
 const html = `<!doctype html>
 <html>
+<html lang="en">
+<head>
+  <div id="header"></div>
+ <script>
+    // Load header
+    fetch('/partials/header.html')
+      .then(res => res.text())
+      .then(html => document.getElementById('header').innerHTML = html);
+
+    // Load footer
+    fetch('/partials/footer.html')
+      .then(res => res.text())
+      .then(html => document.getElementById('footer').innerHTML = html);
+  </script>
+  <script>
+  // Load recent
+    fetch('/partials/Recent.html')
+      .then(res => res.text())
+      .then(html => document.getElementById('recent').innerHTML = html);
+  </script>
+  <meta charset="UTF-8" />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://matchamakes.net/style.css" />
+</head>
+
 <body>
 <h1>Blog Posts</h1>
 <ul>
 ${links}
 </ul>
+<!-- 100% privacy-first analytics -->
+<script async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+
 </body>
+
+  <!-- Footer -->
+  <div id="footer"></div>
 </html>`;
 
 fs.writeFileSync(path.join(outDir, "index.html"), html);
