32a547828932c77b944eb4b78efe590fc7a42e7e
Matcha <96433082+Chatmanm1@users.noreply.github.com>
2025-12-30 09:16:18 -0600

    Update generate_blog_index.js

--- scripts/generate_blog_index.js
+++ scripts/generate_blog_index.js
@@ -62,8 +62,7 @@ const html = `<!DOCTYPE html>
   <script>
     fetch('/partials/header.html').then(r=>r.text()).then(html=>document.getElementById('header').innerHTML=html);
     fetch('/partials/footer.html').then(r=>r.text()).then(html=>document.getElementById('footer').innerHTML=html);
-    fetch('/partials/Recent.html').then(r=>r.text()).then(html=>document.getElementById('recent').innerHTML=html);
-  </script>
+      </script>
 
 </body>
 </html>`;
